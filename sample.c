54a63b30a6910136a305769b6467d124f11a125d
XDEX <rc840327@gmail.com>
2017-06-20 19:30:08 +0800

    add checkio

--- sample.c
+++ sample.c
@@ -11,18 +11,19 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 
 static int width;
 static int height;
 static int cx = 3;
 static int cy = 3;
-static const int bx = 8;
-static const int by = 8;
+
 
 void restart();
 void update(int, int, int);
 void modify(int*, int*, int);
+bool checkIO(int);
 
 
 char buf[1024];
@@ -31,57 +32,62 @@ int main(int argc, char* argv[]) {
 
 	int isS = false;
 	char *port = NULL;
-	char *ip = "127.0.0.1";
-	if (argc > 1){
-		if ( !strcmp(argv[1],"-s") ){
-			isS = true;
-			port = argv[2];
-		}
-		else if( !strcmp(argv[1],"-c") ) {
-			isS = false;
-			char *tmp = argv[2];
-			char *ptr = strtok(tmp,":");
-			ip = ptr;
-			ptr = strtok(NULL,":");
-			port = ptr;
-			printf("IP %s, port %s\n",ip, port);
-		}
+	char *ip = NULL;
+	if (argc != 3) return 0;
+	if ( !strcmp(argv[1],"-s") ){
+		isS = true;
+		port = argv[2];
+	}
+	else if( !strcmp(argv[1],"-c") ) {
+		isS = false;
+		char *tmp = argv[2];
+		char *ptr = strtok(tmp,":");
+		ip = ptr;
+		ptr = strtok(NULL,":");
+		port = ptr;
+		printf("IP %s, port %s\n",ip, port);
+	}
+	
+
+	int sockfd, clientfd, fd;
+	sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if ( sockfd < 0 ){
+		printf("create socket fail\n");
+		return 0;
 	}
 
-	int sockfd, clientfd;
 	struct sockaddr_in dest, client;
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	dest.sin_family = AF_INET;
+	bzero(&dest,sizeof(dest));
+	dest.sin_family = PF_INET;
 	dest.sin_port = htons(atoi(port) );
-	dest.sin_addr.s_addr = inet_addr(ip);
+	dest.sin_addr.s_addr = isS? INADDR_ANY : inet_addr(ip);
 
-	//set nonblocking
-	//fcntl(sockfd, F_SETFL, O_NONBLOCK);
 	if (isS) {
+		if ( bind(sockfd, (struct sockaddr *)&dest, sizeof(dest)) < 0 ) {
+			printf("can't bind server. QQ\n");
+			return 0;
+		}
 		listen(sockfd, 5);
-		bind(sockfd, (struct sockaddr *)&dest, sizeof(dest));
-		printf("server listen on %s with port %s\n",ip, port);
-		socklen_t len = sizeof( struct sockaddr_in );
-		clientfd = accept(sockfd, (struct sockaddr*)&client, &len );
-		sprintf(buf,"hihi client!!\n");
-		send(clientfd, buf, sizeof(buf), 0);
-		recv(clientfd, buf, sizeof(buf), 0);
-		printf("%s~\n",buf);
+		printf("server listen on port %s\n", port);
+		socklen_t client_len = sizeof( client );
+		clientfd = accept(sockfd, (struct sockaddr*)&client, &client_len );
+
+		if ( clientfd < 0 ){
+			printf("No client connect. OAO\n");
+			return 0;
+		}
+		
 	}
 	else{
 		int c = connect(sockfd, (struct sockaddr*)&dest, sizeof(dest) );
 		if ( c < 0 ) {
 			printf("No server to connect. QAQ\n");
+			return 0;
 		}
-		recv(sockfd, buf, sizeof(buf), 0);
-		printf("%s\n", buf);
-		sprintf(buf,"hihi server@@\n");
-		send(sockfd, buf, sizeof(buf), 0);
 	}
 	
-	puts("GG");
-	return 0;
+	fd = isS? clientfd : sockfd;
+
 	initscr();			// start curses mode 
 	getmaxyx(stdscr, height, width);// get screen size
 
@@ -95,11 +101,14 @@ int main(int argc, char* argv[]) {
 	init_colors();
 
 	restart();
+
 	int moved, ch, isQuit = 0;
 	while(true) {			// main loop
 		ch = getch();
 		moved = 0;
-		//printf("%d \n",ch);
+		if ( check_fd(0) == true ){
+			
+		}
 		switch(ch) {
 			case ' ':
 				board[cy][cx] = PLAYER1;
@@ -169,6 +178,29 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
+bool checkIO(int fd) {
+
+	struct timeval tv;
+	fd_set readfds;
+
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+
+	FD_ZERO(&readfds);
+	FD_SET(fd, &readfds);
+
+	// don't care about writefds and exceptfds:
+	if ( select(fd+1, &readfds, NULL, NULL, &tv) < 0 ) {
+		printf("select error. QAQ\n");
+	}
+
+	return FD_ISSET(fd, &readfds);
+
+	// if (FD_ISSET(fd, &readfds))
+	// 	printf("A key was pressed!\n");
+	// else
+	// 	printf("Timed out.\n");
+}
 
 bool findPlayer(int x, int y, int direct, int player) {
 	// 0  1  2  3  4  5  6  7 
@@ -187,7 +219,7 @@ void update(int cx, int cy, int player) {
 		//printf("%d %d %d %d \n", cx, cy, i, findPlayer(cx, cy, i, player));
 		if( findPlayer(cx, cy, i, player) ){
 			x = cx, y = cy;
-			while( 0 <= x && x < bx && 0 <= y && y < by ){
+			while( 0 <= x && x < BOARDSZ && 0 <= y && y < BOARDSZ ){
 				modify(&x,&y,i);
 				//printf("\rx %d y %d i %d\n", x,y,i);
 				if ( board[y][x] == 0-player ) {
@@ -202,7 +234,6 @@ void update(int cx, int cy, int player) {
 	}
 	
 }
-
 
 void restart() {
 	clear();
